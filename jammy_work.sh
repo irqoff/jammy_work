fc6457e3d1f03e0c9e8ca566755bc8d08a11c8ec
Aleksandr Usov <irqoff@gmail.com>
2022-04-13 23:16:36 +0300

    fix typo

--- jammy_work.sh
+++ jammy_work.sh
@@ -10,7 +10,7 @@ set +o nounset
 source ~/.bashrc
 set -o nounset
 
-sudo -u "$USER" bash -ci 'cd ~/jammy_work && ansible-playbook jammy.yml -K -t brew_package --skit-tags go'
+sudo -u "$USER" bash -ci 'cd ~/jammy_work && ansible-playbook jammy.yml -K -t brew_package --skip-tags go'
 sudo -u "$USER" bash -ci 'cd ~/jammy_work && ansible-playbook jammy.yml -K -t go'
 
 set +o errexit +o nounset +o xtrace
